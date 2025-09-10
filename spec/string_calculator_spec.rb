4fae4c6e83a58995b0845ac812ef9614a120c3e8
029shreya <shirkes088@gmail.com>
2025-09-10 12:53:32 +0530

    second step

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-  describe "#add" do
-    it "returns 0 for an empty string" do
+  describe '#add' do
+    it 'returns 0 for an empty string' do
       calc = StringCalculator.new
-      expect(calc.add("")).to eq(0)
+      expect(calc.add('')).to eq(0)
+    end
+    it 'returns the number itself if only one number' do
+      calc = StringCalculator.new
+      expect(calc.add('1')).to eq(1)
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 class StringCalculator
   def add(numbers)
+    return 0 if numbers.empty?
+    return numbers.to_i if numbers.match?(/^\d+$/)
+
     0
   end
 end
